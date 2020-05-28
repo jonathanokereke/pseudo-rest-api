33f8ea5c7ac60860b3dd2631baed3206a0e3daf1
Jonathan <jonathanokereke@web.de>
2020-05-28 08:27:19 +0200

    Swagger enabled

--- src/main/java/de/uniklinik/medic/pseudo/Application.java
+++ src/main/java/de/uniklinik/medic/pseudo/Application.java
@@ -8,11 +8,13 @@ import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by Jonathan on 10.Apr.2020 . 5:02 PM
  */
 
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
